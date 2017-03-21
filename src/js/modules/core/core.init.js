c489bc87832dad1f8510736952cefe65ebb60b3d
Peter Anderson <diniremix@gmail.com>
2017-03-20 22:06:28 -0500

    new fn go, broadcast for $stateNotFound, $stateChangeError, check debug state

--- src/js/modules/core/core.init.js
+++ src/js/modules/core/core.init.js
@@ -2,7 +2,7 @@
 (function() {
     'use strict';
 
-    function settingsRun($rootScope, $state, $log, $localStorage) {
+    function settingsRun($rootScope, $state, $log, $localStorage, APP_CONFIG) {
         $rootScope.app = {
             name: 'MaterializeApp',
             description: ' - Materialize App',
@@ -12,6 +12,29 @@
             viewAnimation: 'ng-fadeInUp',
             background: 'cyan darken-1'
         };
+
+		$rootScope.go = function (path) {
+			$state.go(path);
+		}
+
+		$rootScope.$on('$stateNotFound', function (event, notFoundState){
+			event.preventDefault();
+			$log.warn('state not found', notFoundState.to);
+
+			if (APP_CONFIG.DEBUG){
+				$log.warn(notFoundState.toParams);
+				$log.warn(notFoundState.options);
+			}
+		});
+
+		$rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error){
+			event.preventDefault();
+			$log.error('state error', toState);
+
+			if (APP_CONFIG.DEBUG){
+				$log.error(error);
+			}
+		});
     };
 
 	angular.module('app.core').run(settingsRun);
@@ -19,6 +42,7 @@
 		'$rootScope',
 		'$state',
 		'$log',
-		'$localStorage'
+		'$localStorage',
+		'APP_CONFIG'
 	];
 })();
